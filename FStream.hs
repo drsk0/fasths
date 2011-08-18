8ea7a3a1d44b02ff921bb467ada290cb394a57c8
Robin Krom <robin.krom23@gmail.com>
2011-08-18 22:38:36 +0100

    cosmetics: FStream is hlint save.

--- FStream.hs
+++ FStream.hs
@@ -10,19 +10,17 @@ import FParser
 -- |Example of a stream. In this case, the stream consists of messages
 -- and the parser is reset when a new message starts.
 stream::Templates -> (Int -> String) -> A.Parser [(NsName, Maybe FValue)]
-stream ts tid2temp = evalStateT (FStream.reset ts >> (A.many (message ts tid2temp))) (initState ts)
+stream ts tid2temp = evalStateT (FStream.reset ts >> A.many (message ts tid2temp)) (initState ts)
 
 -- |Stateful parser for one message depending on templates and the tid2temp 
 -- converter function.
 message::Templates -> (Int -> String) -> StateT FState A.Parser (NsName, Maybe FValue)
 message ts tid2temp = let   env = initEnv ts tid2temp 
-    in do 
-        runReaderT segment' env
+    in runReaderT segment' env
 
 -- |Resets the state of a parser to the initial state depending on the
 -- templates.
 reset:: Templates -> StateT FState A.Parser ()
-reset ts = do 
-    put (initState ts)
+reset ts = put (initState ts)
 
 -- NOTE: Blocks are not supported at the time.
