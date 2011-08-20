aa14180f70699021c4390620873bb7c499acc33f
Robin Krom <robin.krom23@gmail.com>
2011-08-20 12:47:51 +0100

    feature: FStream compiles without warnings.

--- FStream.hs
+++ FStream.hs
@@ -3,19 +3,18 @@ module FStream where
 import qualified Data.Attoparsec.Char8 as A
 import Control.Monad.State
 import Control.Monad.Reader
-import Control.Applicative 
 import FAST
 import FParser 
 
 -- |Example of a stream. In this case, the stream consists of messages
 -- and the parser is reset when a new message starts.
 stream::Templates -> (Int -> String) -> A.Parser [(NsName, Maybe FValue)]
-stream ts tid2temp = evalStateT (FStream.reset ts >> A.many (message ts tid2temp)) (initState ts)
+stream ts tid2tem = evalStateT (FStream.reset ts >> A.many (message ts tid2tem)) (initState ts)
 
 -- |Stateful parser for one message depending on templates and the tid2temp 
 -- converter function.
 message::Templates -> (Int -> String) -> StateT FState A.Parser (NsName, Maybe FValue)
-message ts tid2temp = let   env = initEnv ts tid2temp 
+message ts tid2tem = let   env = initEnv ts tid2tem 
     in runReaderT segment' env
 
 -- |Resets the state of a parser to the initial state depending on the
