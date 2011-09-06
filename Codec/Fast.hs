f517dce07ed086ed1297789fedfc65ff2916496f
Robin Krom <robin.krom23@gmail.com>
2011-09-06 20:18:19 +0100

    feature: moved stream examples to parseFile example.

--- Codec/Fast.hs
+++ Codec/Fast.hs
@@ -1,4 +1,4 @@
-module Codec.Fast where 
+module Codec.Fast (message, Codec.Fast.reset) where
 
 import qualified Data.Attoparsec as A
 import Control.Applicative
@@ -7,24 +7,6 @@ import Control.Monad.Reader
 import Data.Word (Word32)
 import Codec.Fast.Data
 import Codec.Fast.Parser 
-
--- |Example of a stream. This stream never resets its context.
-stream0::Templates -> (Word32 -> String) -> A.Parser [(NsName, Maybe FValue)]
-stream0 ts tid2tem = evalStateT (A.many1 (message ts tid2tem)) (initState ts)
-
--- |Example of a stream. In this case, the stream consists of messages
--- and the parser is reset when a new message starts.
-stream1::Templates -> (Word32 -> String) -> A.Parser [(NsName, Maybe FValue)]
-stream1 ts tid2tem = evalStateT (A.many1 (Codec.Fast.reset ts >> message ts tid2tem)) (initState ts)
-
--- |Example of a stream. This stream resets the state on every occurence of a message with
--- name "Reset".
-stream2::Templates -> (Word32 -> String) -> A.Parser [(NsName, Maybe FValue)]
-stream2 ts tid2tem = evalStateT (A.many1 (resetMsg <|> msg)) (initState ts)
-    where    msg = message ts tid2tem
-             resetMsg =  do 
-                    m@(NsName (NameAttr n) _ _, _) <- msg
-                    if n == "Reset" then Codec.Fast.reset ts >> return m else fail "Not reset."
 
 -- |Stateful parser for one message depending on templates and the tid2temp 
 -- converter function.
